369e7776d028fe0c7c3e1073c4bcebdfd0b814ab
KejviBode <trainee.kejvi.bode@sigmalabs.co.uk>
2023-04-09 21:06:44 +0100

    Complete test_2

--- DE Test April 2022/test_2.py
+++ DE Test April 2022/test_2.py
@@ -1,3 +1,5 @@
+import json
+import requests as req
 # A team of analysts wish to discover how far people are travelling to their nearest
 # desired court. We have provided you with a small test dataset so you can find out if
 # it is possible to give the analysts the data they need to do this. The data is in
@@ -69,6 +71,75 @@
 # - the dx_number (if available) of the nearest court of the right type
 # - the distance to the nearest court of the right type
 
+
+def get_people_info(file_name: str) -> list[dict]:
+    '''Retrieves all people's info from csv file with format
+    "name", "home_postcode", and "looking_for_court_type"
+    '''
+    people_info = []
+    with open(file_name) as data:
+        next(data)
+        for line in data:
+            person = {}
+            individual_data = line.split(",")
+            person["name"] = individual_data[0].strip()
+            person["home_postcode"] = individual_data[1].strip()
+            person["looking_for_court_type"] = individual_data[2].strip()
+            people_info.append(person)
+    return people_info
+
+
+def get_court_api_info(postcode: str) -> json:
+    '''Retrieves all data from API from the specified postcode'''
+    url = f"https://courttribunalfinder.service.gov.uk/search/results.json?postcode={postcode}"
+    response = req.get(url, timeout=5)
+    court_data = response.json()
+    if len(court_data) < 1:
+        raise ValueError(
+            "No courts found for that postcode, check if correct postcode")
+    return court_data
+
+
+def get_closest_correct_court(court_json: list[dict], court_type) -> dict:
+    '''Retrieves to closest court with correct typing from the json data
+    extracted from the API
+    '''
+    correct_court_type = [
+        court for court in court_json if court_type in court["types"]]
+    if len(correct_court_type) < 1:
+        raise ValueError("No courts found with desired type")
+    min_distance_court = correct_court_type[0]
+    min_distance = min_distance_court["distance"]
+    for court in correct_court_type:
+        if court["distance"] < min_distance:
+            min_distance = court["distance"]
+            min_distance_court = court
+    return min_distance_court
+
+
+def combine_person_with_court(person: dict, correct_court: dict) -> dict:
+    person["nearest_court_of_right_type"] = correct_court["name"]
+    if correct_court["dx_number"] is not None:
+        person["dx_number"] = correct_court["dx_number"]
+    person["distance_to_court"] = correct_court["distance"]
+    return person
+
+
+def find_correct_courts_for_people(people: list[dict]) -> list[dict]:
+    courts_found = []
+    for person in people:
+        courts = get_court_api_info(person["home_postcode"])
+        correct_court = get_closest_correct_court(
+            courts, person["looking_for_court_type"])
+        final_person = combine_person_with_court(person, correct_court)
+        courts_found.append(person)
+    return courts_found
+
+
 if __name__ == "__main__":
     # [TODO]: write your answer here
-    pass
+    people_main = get_people_info("people.csv")
+    api = get_court_api_info("NP108XG")
+    print(get_closest_correct_court(api, "Crown Court"))
+
+    print(find_correct_courts_for_people(people_main))
